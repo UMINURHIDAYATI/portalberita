8e64636bfbe44f7f171082bc40adeb36a910cae4
UMINURHIDAYATI <uminurhidayati012@gmail.com>
2021-12-23 21:43:41 +0700

    UAS

--- php/src/connect.php
+++ php/src/connect.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
+$servername = "172.20.0.2";
 $username = "root";
-$password = "";
-$basename = "projekt";
+$password = "admin";
+$basename = "projek";
 
 $dbc = mysqli_connect($servername, $username, $password, $basename) or die("Connection to DB failed");
 if($dbc){
